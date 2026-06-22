5baba7444a898bd0a466da82ce439a0b6acc39ac
nusrat <nusratchy.002@gmail.com>
2026-06-22 20:51:21 +0600

    fixed footer section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
 import "./globals.css";
-import Footer from "@/components/Shared/Footer";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -8,8 +8,8 @@ import {
 
 export default function Footer() {
   return (
-    <footer className="bg-blue-950 text-white mt-20">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+    <footer className="bg-blue-950 text-white mt-28">
+      <div className="max-w-7xl mx-auto px-6 sm:px-8 lg:px-8 py-16">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
           
           <div>
@@ -84,14 +84,14 @@ export default function Footer() {
               Subscribe to receive ebook updates.
             </p>
 
-            <div className="flex">
+            <div className="flex flex-col sm:flex-row gap-3">
               <input
                 type="email"
                 placeholder="Your Email"
                 className="w-full px-4 py-3 rounded-l-lg text-black outline-none"
               />
 
-              <button className="bg-blue-600 px-5 rounded-r-lg hover:bg-blue-700 transition">
+              <button className="bg-blue-600 px-5 py-3 rounded-lg sm:rounded-r-lg sm:rounded-l-none hover:bg-blue-700 transition">
                 Join
               </button>
             </div>
