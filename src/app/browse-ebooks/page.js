49ad85567bad15e24531ee07c858005e301d19b1
nusrat <nusratchy.002@gmail.com>
2026-06-23 21:08:49 +0600

    fix books grid alignment

--- src/app/browse-ebooks/page.js
+++ src/app/browse-ebooks/page.js
@@ -30,7 +30,7 @@ export default function BrowsePage() {
         <EbookFilters />
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 ">
         {ebooks.map((ebook) => (
           <EbookCard
             key={ebook._id}
