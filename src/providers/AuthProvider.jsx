1cf35a38021cd88a7331beca099fa17d2685614f
nusrat <nusratchy.002@gmail.com>
2026-06-27 06:05:53 +0600

    Fix AuthProvider

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,11 +1,5 @@
 "use client";
 
-import { SessionProvider } from "better-auth/react";
-
 export default function AuthProvider({ children }) {
-  return (
-    <SessionProvider>
-      {children}
-    </SessionProvider>
-  );
+  return children;
 }
